8c0ff9eb3373ce998a68754aad697e121720b962
Anton <115165322+j0nt0n@users.noreply.github.com>
2024-11-13 14:20:58 +0300

    Update

--- CafeOrder/app/build.gradle.kts
+++ CafeOrder/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("androidx.navigation.safeargs")
 }
 
 android {
@@ -33,9 +34,6 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-    buildFeatures {
-        viewBinding = true
-    }
 }
 
 dependencies {
@@ -43,10 +41,24 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.navigation.fragment.ktx)
-    implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    val nav_version = "2.8.0" // Версия библиотеки Navigation
+
+    // Интеграция с Jetpack Compose (если используется)
+    implementation("androidx.navigation:navigation-compose:$nav_version")
+
+    // Интеграция с Views/Fragments
+    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
+
+    // Поддержка Feature module для Fragments (если используется)
+    implementation("androidx.navigation:navigation-dynamic-features-fragment:$nav_version")
+
+    // Тестирование Navigation (если используется)
+    androidTestImplementation("androidx.navigation:navigation-testing:$nav_version")
 }
